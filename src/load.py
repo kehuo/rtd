f829680cf0406ad00b106c423ec78daf790b2b1a
huoke <huoke@baidu.com>
2022-07-20 17:22:22 +0800

    big change

---
+++ src/load.py
@@ -0,0 +1,19 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+class Loader(object):
+    def __init__(self):
+        pass
+
+    def load(self, path: str) -> str:
+        """
+        The load method.
+
+        Args:
+            path(str): path to laod file.
+
+        Returns:
+            str: loaded data.
+        """
+        return "loaded model"
